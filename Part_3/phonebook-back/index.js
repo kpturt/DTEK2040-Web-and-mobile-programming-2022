fc62f06f1bb6d06e2eb3f51c111521a6245925e4
Kari Turtiainen <kp.turtiainen7@gmail.com>
2022-04-12 20:04:42 +0300

    small fixes

--- Part_3/phonebook-back/index.js
+++ Part_3/phonebook-back/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.static('build'))
 const bodyParser = require('body-parser')
 const { response, request } = require('express')
 app.use(bodyParser.json())
